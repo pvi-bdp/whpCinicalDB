5a04fab6f4a5da719383d1ae10833b1f98fa3814
Michael Goldsby <mgoldsby@puravida-institute.org>
2021-03-07 13:17:11 -0800

    initial push

--- load_cerner_dim.sql
+++ load_cerner_dim.sql
@@ -8,54 +8,199 @@ set hive.support.concurrency=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
 set hive.txn.manager=org.apache.hadoop.hive.ql.lockmgr.DbTxnManager;
 
-
 ------------------------------------
---  diagnosis table creation
+--  HF_D_ADMISSION_SOURCE table creation
 ------------------------------------
-DROP TABLE IF EXISTS diagnosis_txt PURGE;
-CREATE TABLE diagnosis_txt (
-        encounter_id string,
-        diagnosis_id string,
-        diagnosis_priority string,
-        diagnosis_type_id string,
-        present_on_admit_id string,
-        third_party_ind string)
+DROP TABLE IF EXISTS d_admission_source_txt PURGE;
+CREATE TABLE d_admission_source_txt (
+    ADMISSION_SOURCE_ID string,
+    ADMISSION_SOURCE_CODE string,
+    ADMISSION_SOURCE_CODE_DESC string)
 ROW FORMAT DELIMITED
   FIELDS TERMINATED BY '|'
 STORED AS TEXTFILE
 LOCATION
-  'hdfs://namenode:8020/da-data/public/diagnosis/txt'
+  'hdfs://namenode:8020/da-data/cerner/d_admission_source/txt'
 TBLPROPERTIES(
     'external.table.purge'='true'
 );
 
-DROP TABLE IF EXISTS diagnosis PURGE;
-CREATE EXTERNAL TABLE diagnosis (
-        encounter_id string,
-        diagnosis_id string,
-        diagnosis_priority string,
-        diagnosis_type_id string,
-        present_on_admit_id string,
-        third_party_ind string )
+DROP TABLE IF EXISTS d_admission_source PURGE;
+CREATE EXTERNAL TABLE d_admission_source (
+    ADMISSION_SOURCE_ID string,
+    ADMISSION_SOURCE_CODE string,
+    ADMISSION_SOURCE_CODE_DESC string)
 ROW FORMAT DELIMITED
   FIELDS TERMINATED BY '|'
 STORED AS ORC
 LOCATION
-  'hdfs://namenode:8020/da-data/public/diagnosis/orc'
+  'hdfs://namenode:8020/da-data/cerner/d_admission_source/orc'
 TBLPROPERTIES(
     "orc.compress"="SNAPPY",
     "external.table.purge"="true"
 );
 
 
-alter table diagnosis_txt set tblproperties('EXTERNAL'='FALSE');
-TRUNCATE TABLE diagnosis_txt;
-alter table diagnosis_txt set tblproperties('EXTERNAL'='TRUE');
+alter table d_admission_source_txt set tblproperties('EXTERNAL'='FALSE');
+TRUNCATE TABLE d_admission_source_txt;
+alter table d_admission_source_txt set tblproperties('EXTERNAL'='TRUE');
 
-alter table diagnosis set tblproperties('EXTERNAL'='FALSE');
-TRUNCATE TABLE diagnosis;
-alter table diagnosis set tblproperties('EXTERNAL'='TRUE');
+alter table d_admission_source set tblproperties('EXTERNAL'='FALSE');
+TRUNCATE TABLE d_admission_source;
+alter table d_admission_source set tblproperties('EXTERNAL'='TRUE');
 
-LOAD DATA LOCAL INPATH '/opt/data/input/_codes/HF_F_Diagnosis_2008_sample.dat' INTO Table diagnosis_txt;
-INSERT OVERWRITE TABLE diagnosis SELECT * FROM diagnosis_txt;
+LOAD DATA LOCAL INPATH '/opt/data/input/cerner/HF_D_ADMISSION_SOURCE.dat' INTO Table d_admission_source_txt;
+INSERT OVERWRITE TABLE d_admission_source SELECT * FROM d_admission_source_txt;
+
+DROP TABLE IF EXISTS d_admission_source_txt PURGE;
+
+------------------------------------
+--  HF_D_ADMISSION_TYPE table creation
+------------------------------------
+DROP TABLE IF EXISTS d_admission_type_txt PURGE;
+CREATE TABLE d_admission_type_txt (
+    ADMISSION_TYPE_ID string,
+    ADMISSION_TYPE_CODE string,
+    ADMISSION_TYPE_CODE_DESC string)
+ROW FORMAT DELIMITED
+  FIELDS TERMINATED BY '|'
+STORED AS TEXTFILE
+LOCATION
+  'hdfs://namenode:8020/da-data/cerner/d_admission_type/txt'
+TBLPROPERTIES(
+    'external.table.purge'='true'
+);
+
+DROP TABLE IF EXISTS d_admission_type PURGE;
+CREATE EXTERNAL TABLE d_admission_type (
+    ADMISSION_TYPE_ID string,
+    ADMISSION_TYPE_CODE string,
+    ADMISSION_TYPE_CODE_DESC string)
+ROW FORMAT DELIMITED
+  FIELDS TERMINATED BY '|'
+STORED AS ORC
+LOCATION
+  'hdfs://namenode:8020/da-data/cerner/d_admission_type/orc'
+TBLPROPERTIES(
+    "orc.compress"="SNAPPY",
+    "external.table.purge"="true"
+);
+
+
+alter table d_admission_type_txt set tblproperties('EXTERNAL'='FALSE');
+TRUNCATE TABLE d_admission_type_txt;
+alter table d_admission_type_txt set tblproperties('EXTERNAL'='TRUE');
+
+alter table d_admission_type set tblproperties('EXTERNAL'='FALSE');
+TRUNCATE TABLE d_admission_type;
+alter table d_admission_type set tblproperties('EXTERNAL'='TRUE');
+
+LOAD DATA LOCAL INPATH '/opt/data/input/cerner/HF_D_ADMISSION_TYPE.dat' INTO Table d_admission_type_txt;
+INSERT OVERWRITE TABLE d_admission_type SELECT * FROM d_admission_type_txt;
+
+DROP TABLE IF EXISTS d_admission_type_txt PURGE;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+------------------------------------
+--  diagnosis dimension table creation
+------------------------------------
+DROP TABLE IF EXISTS d_diagnosis_txt PURGE;
+CREATE TABLE d_diagnosis_txt (
+        diagnosis_id string,
+        diagnosis_type string,
+        diagnosis_code string,
+        diagnosis_description string)
+ROW FORMAT DELIMITED
+  FIELDS TERMINATED BY '|'
+STORED AS TEXTFILE
+LOCATION
+  'hdfs://namenode:8020/da-data/cerner/d_diagnosis/txt'
+TBLPROPERTIES(
+    'external.table.purge'='true'
+);
+
+DROP TABLE IF EXISTS d_diagnosis PURGE;
+CREATE EXTERNAL TABLE d_diagnosis (
+        diagnosis_id string,
+        diagnosis_type string,
+        diagnosis_code string,
+        diagnosis_description string)
+ROW FORMAT DELIMITED
+  FIELDS TERMINATED BY '|'
+STORED AS ORC
+LOCATION
+  'hdfs://namenode:8020/da-data/cerner/d_diagnosis/orc'
+TBLPROPERTIES(
+    "orc.compress"="SNAPPY",
+    "external.table.purge"="true"
+);
+
+
+alter table d_diagnosis_txt set tblproperties('EXTERNAL'='FALSE');
+TRUNCATE TABLE d_diagnosis_txt;
+alter table d_diagnosis_txt set tblproperties('EXTERNAL'='TRUE');
+
+alter table d_diagnosis set tblproperties('EXTERNAL'='FALSE');
+TRUNCATE TABLE d_diagnosis;
+alter table d_diagnosis set tblproperties('EXTERNAL'='TRUE');
+
+LOAD DATA LOCAL INPATH '/opt/data/input/cerner/HF_D_DIAGNOSIS.dat' INTO Table d_diagnosis_txt;
+INSERT OVERWRITE TABLE d_diagnosis SELECT * FROM d_diagnosis_txt;
 
